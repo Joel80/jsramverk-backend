91ea3c5658633ca2ecfcca7d46015db2703026a7
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-06 11:59:54 +0200

    Model for docs

---
+++ models/docs.js
@@ -0,0 +1,87 @@
+/**
+ * Model for docs
+ */
+
+const database = require('../db/database');
+const ObjectId = require("mongodb").ObjectId;
+
+const docs = {
+    getAllDocs: async function getAllDocs(req, res) {
+        let db;
+
+        try {
+            db = await database.getDb();
+            //console.log(db);
+            const resultset = await db.collection.find({}).toArray();
+            console.log(resultset);
+
+            return res.status(200).json({data: resultset});
+
+        } catch (error) {
+            return res.status(500).json({
+                status: 500,
+                title: "Database error",
+                message: error.message
+            });
+        } finally {
+            await db.client.close();
+        }
+    },
+
+    saveDoc: async function saveDoc(req, res) {
+        let db;
+        let doc = {};
+        let name = req.body.name;
+        let html = req.body.html;
+
+        doc.name = name;
+        doc.html = html;
+
+        try {
+            db = await database.getDb();
+            const result = await db.collection.insertOne(doc);
+            
+            // If there is a result return status 201 and inserted id
+            if (result) {
+                return res.status(201).json({id: result.insertedId });
+            }
+
+        } catch (error) {
+            return res.status(500).json({
+                status: 500,
+                title: "Database error",
+                message: error.message
+            });
+        } finally {
+            await db.client.close();
+        }
+    },
+
+    updateDoc: async function updateDoc(req, res) {
+        let db;
+
+        console.log(req.body._id);
+        const filter = { _id: ObjectId(req.body._id) };
+        
+        const updateDoc = { $set: {name: req.body.name, html: req.body.html}, };
+
+        try {
+            db = await database.getDb();
+            const result = await db.collection.updateOne(filter, updateDoc);
+            
+            //res.send("Ok");
+            res.status(200).json({message:"Successfully updated"});
+
+        } catch (error) {
+            return res.status(500).json({
+                status: 500,
+                title: "Database error",
+                message: error.message
+            });
+        } finally {
+            await db.client.close();
+        }
+    },
+}
+
+module.exports = docs;
