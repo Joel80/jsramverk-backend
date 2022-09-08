92c8b7e2f9ff20d2984b1ef62bfe2618bb059d97
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-08 10:14:54 +0200

    New db dsn

--- db/database.js
+++ db/database.js
@@ -8,19 +8,23 @@ const localhost = name + '.local';
 
 const database = {
     getDb: async function getDb () {
-        let dsn =
-        `mongodb://${localhost}:27017/docs` || `mongodb://localhost:27017/docs`;
+        let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.5vdprdp.mongodb.net/?retryWrites=true&w=majority`
+        /* `mongodb://${localhost}:27017/docs` || `mongodb://localhost:27017/docs`; */
 
         if (process.env.NODE_ENV === 'test') {
             dsn = `mongodb://${localhost}:27017/test` || `mongodb://localhost:27017/test`;
         }
 
-        const client = await mongo.connect(dsn);
+        const client = await mongo.connect(dsn, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
 
         const db = await client.db();
         const collection = await db.collection(collectionName);
 
         return {
+            db: db,
             collection: collection,
             client: client,
         };
--- src/setup.js
+++ src/setup.js
@@ -7,8 +7,8 @@ const localhost = name + '.local';
 console.log(localhost);
 
 const mongo = require("mongodb").MongoClient;
-const dsn =
-`mongodb://${localhost}:27017/docs` || `mongodb://localhost:27017/docs`;
+const dsn = "mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.5vdprdp.mongodb.net/?retryWrites=true&w=majority";
+/* `mongodb://${localhost}:27017/docs` || `mongodb://localhost:27017/docs`; */
 
 const documentModel = require('../models/docs');
 
