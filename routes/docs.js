ab23d7b3c402661c58c1d4ceef4f2a6b5bf80da1
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-08 07:32:32 +0200

    Added route for PUT

--- routes/docs.js
+++ routes/docs.js
@@ -9,6 +9,8 @@ router.get("/:id", (req, res) => docsModel.getOneDocById(req, res));
 
 router.post("/", (req, res) => docsModel.saveDoc(req, res));
 
+router.put("/", (req, res) => docsModel.updateDoc(req, res));
+
 router.post("/update", (req, res) => docsModel.updateDoc(req, res));
 
 module.exports = router;
