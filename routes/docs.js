ee325b3f92ec6c3f09d5c8b7ee1b6184e6dc0059
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-10 07:52:21 +0200

    Sipmlifying route handlers

--- routes/docs.js
+++ routes/docs.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const docsModel = require('../models/docs');
 
-router.get("/", (req, res) => docsModel.getAllDocs(req, res));
+router.get("/", (req, res) => docsModel.getAllDocs(res));
 
-router.get("/:id", (req, res) => docsModel.getOneDocById(req, res));
+router.get("/:id", docsModel.getOneDocById(req, res));
 
-router.post("/", (req, res) => docsModel.saveDoc(req, res));
+router.post("/", docsModel.saveDoc(req, res));
 
-router.put("/", (req, res) => docsModel.updateDoc(req, res));
+router.put("/",  docsModel.updateDoc(req, res));
 
-router.post("/update", (req, res) => docsModel.updateDoc(req, res));
+router.post("/update", docsModel.updateDoc(req, res));
 
 module.exports = router;
