49368f73339e59d7c625868e910c28f8b492c8f1
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-13 12:54:51 +0200

    Setting up first test

---
+++ test/test.js
@@ -0,0 +1,53 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app.js');
+
+chai.should();
+
+chai.use(chaiHttp);
+
+console.log(process.env.NODE_ENV);
+
+const database = require("../db/database.js");
+const collectionName = "documents";
+
+describe('Documents', () => {
+    before(() => {
+        return new Promise(async (resolve) => {
+            const db = await database.getDb();
+
+            db.db.listCollections(
+                { name: collectionName }
+            )
+                .next()
+                .then(async function (info) {
+                    if (info) {
+                        await db.collection.drop();
+                    }
+                })
+                .catch(function (err) {
+                    console.error(err);
+                })
+                .finally(async function () {
+                    await db.client.close();
+                    resolve();
+                });
+        });
+    });
+
+
+    it('200 HAPPY PATH', (done) => {
+        chai.request(server)
+            .get("/docs")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an("object");
+                res.body.data.should.be.an("array");
+                res.body.data.length.should.be.equal(0);
+
+                done();
+            });
+    });
+});
