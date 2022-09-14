0fe02ebaa78c995160ffcc77bdb1fca8ed8e39e7
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-14 15:26:17 +0200

    Added responses with error codes when parameters are not sufficient

--- models/docs.js
+++ models/docs.js
@@ -57,56 +57,71 @@ const docs = {
     saveDoc: async function saveDoc(req, res) {
         let db;
         let doc = {};
-        let name = req.body.name;
-        let html = req.body.html;
 
-        doc.name = name;
-        doc.html = html;
+        if (req.body.name && req.body.html) {
+            let name = req.body.name;
+            let html = req.body.html;
 
-        try {
-            db = await database.getDb();
-            const result = await db.collection.insertOne(doc);
-            
-            // If there is a result return status 201 and inserted id
-            if (result) {
-                return res.status(201).json({id: result.insertedId });
+            doc.name = name;
+            doc.html = html;
+
+            try {
+                db = await database.getDb();
+                const result = await db.collection.insertOne(doc);
+                
+                // If there is a result return status 201 and inserted id
+                if (result) {
+                    return res.status(201).json({id: result.insertedId });
+                }
+
+            } catch (error) {
+                return res.status(500).json({
+                    status: 500,
+                    title: "Database error",
+                    message: error.message
+                });
+            } finally {
+                await db.client.close();
             }
-
-        } catch (error) {
-            return res.status(500).json({
-                status: 500,
-                title: "Database error",
-                message: error.message
-            });
-        } finally {
-            await db.client.close();
+        } else {
+            return res.status(400).json({errors: {
+                message: "name and html needed to save document"
+            }})
         }
+        
     },
 
     updateDoc: async function updateDoc(req, res) {
         let db;
 
-        console.log(req.body._id);
-        const filter = { _id: ObjectId(req.body._id) };
+        if (req.body._id) {
+            const filter = { _id: ObjectId(req.body._id) };
 
-        const updateDoc = { $set: {name: req.body.name, html: req.body.html}, };
+            const updateDoc = { $set: {name: req.body.name, html: req.body.html}, };
 
-        try {
-            db = await database.getDb();
-            const result = await db.collection.updateOne(filter, updateDoc);
-            
-            //res.send("Ok");
-            res.status(200).json({message:"Successfully updated"});
+            try {
+                db = await database.getDb();
+                const result = await db.collection.updateOne(filter, updateDoc);
+                
+                //res.send("Ok");
+                res.status(200).json({message:"Successfully updated"});
 
-        } catch (error) {
-            return res.status(500).json({
-                status: 500,
-                title: "Database error",
-                message: error.message
-            });
-        } finally {
-            await db.client.close();
+            } catch (error) {
+                return res.status(500).json({
+                    status: 500,
+                    title: "Database error",
+                    message: error.message
+                });
+            } finally {
+                await db.client.close();
+            }
+        } else {
+            return res.status(400).json({errors: {
+                message: "_id needed to update document"
+            }})
         }
+
+
     },
 }
 
