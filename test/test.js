e56c52daa2150703f3261ecc0c7376ab910fbb8c
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-14 14:44:11 +0200

    Added tests for post and put routes

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app.js');
+const ObjectId = require("mongodb").ObjectId;
 
 chai.should();
 
@@ -12,6 +13,9 @@ console.log(process.env.NODE_ENV);
 
 const database = require("../db/database.js");
 const collectionName = "documents";
+
+let _id = "";
+
 
 describe('Documents', () => {
     before(() => {
@@ -37,17 +41,74 @@ describe('Documents', () => {
         });
     });
 
-
-    it('200 HAPPY PATH', (done) => {
-        chai.request(server)
-            .get("/docs")
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.an("object");
-                res.body.data.should.be.an("array");
-                res.body.data.length.should.be.equal(0);
-
-                done();
-            });
+    describe('GET /docs', () => {
+        it('200 HAPPY PATH', (done) => {
+            chai.request(server)
+                .get("/docs")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("object");
+                    res.body.data.should.be.an("array");
+                    res.body.data.length.should.be.equal(0);
+    
+                    done();
+                }
+            );
+        });
     });
+
+    describe('POST /docs', () => {
+        it('Creating new doc', (done) => {
+            let doc = {
+                name: "A name",
+                html: "Some html</br>"
+            }
+
+            chai.request(server)
+                .post("/docs")
+                .send(doc)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("id");
+                    console.log(res.body.id);
+                    _id = res.body.id;
+                    console.log(_id);
+                    done();
+                }
+            );
+        });
+    });
+
+    console.log(`ID: ${_id}`);
+
+    describe('PUT /docs', () => {
+        let result;
+        console.log("Running put");
+        console.log(result);
+        it('should update a doc with given id', (done) => {
+
+            let doc = {
+                _id: _id,
+                name: "A doc",
+                html: "Some html updated"
+            }
+
+            chai.request(server)
+                .put("/docs")
+                .send(doc)
+                .end((err, res) => {
+                    res.should.have.status(204);
+                    res.body.should.be.an("object");
+                    console.log(res.body);
+                    //res.body.should.have.property("message");
+                    done();
+                }
+            );
+    
+            
+  
+        });
+    });
+    
 });
