bd862b6af3e5e5484683fbc99ca1c38cabfa47ea
Joel Löfgren <jdtlofgren@gmail.com>
2022-09-28 09:33:55 +0200

    Added function for checking token

--- models/auth.js
+++ models/auth.js
@@ -150,9 +150,27 @@ const auth = {
                 }
             })
         });
-    }
+    },
 
-    // TODO add function for checking token
+    checkToken: function checkToken(req, res, next) {
+
+        const token = req.headers['x-access-token'];
+
+        jwt.verify(token, process.env.JWT_SECRET, function(err, decoded){
+
+            if (err) {
+                return res.status(401).json({
+                    errors: {
+                        status: 401,
+                        message: "Token not valid",
+                    }
+                })
+            }
+
+            // Token is valid continue
+            next();
+        });
+    }
 }
 
 module.exports = auth;
