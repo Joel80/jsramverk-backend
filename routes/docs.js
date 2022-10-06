758d102e8f8783d952a4f2727722699a67c6b6cf
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-06 08:35:53 +0200

    Removed authModel

--- routes/docs.js
+++ routes/docs.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const docsModel = require('../models/docs');
-const authModel = require('../models/auth');
+//const authModel = require('../models/auth');
 
 
 router.get(
