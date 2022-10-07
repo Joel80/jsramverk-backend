37456d7272d4d74e6c9a748f4698d0f7dd800ac8
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-07 12:46:22 +0200

    Added type for DocInput

---
+++ graphql/docInput.js
@@ -0,0 +1,20 @@
+const {
+    GraphQLInputObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLNonNull,
+} = require('graphql');
+
+const DocInputType = new GraphQLInputObjectType({
+    name: 'DocumentInput',
+    description: 'This represents a document',
+    fields: () => ({
+        _id:  { type: GraphQLString },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        html: { type: new GraphQLNonNull(GraphQLString) },
+        allowed_users: { type: new GraphQLList(GraphQLString) }
+
+    })
+});
+
+module.exports = DocInputType;
