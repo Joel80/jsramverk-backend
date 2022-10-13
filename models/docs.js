ddfe17f56ce238b4721dd344bdf9751011b69002
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-13 15:57:21 +0200

    Added email function

--- models/docs.js
+++ models/docs.js
@@ -4,7 +4,9 @@
 
 const database = require('../db/database');
 const ObjectId = require("mongodb").ObjectId;
-
+const validator = require('email-validator');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const docs = {
     getAllDocs: async function getAllDocs(email) {
@@ -112,7 +114,44 @@ const docs = {
         } finally {
             await db.client.close();
         }
+
+        
     },
+
+    emailNewUser: async function emailNewUser(user, email) {
+        if(!validator.validate(email)) {
+            return {
+                errors: {
+                    status: 400,
+                    title: "Email-error",
+                    message: "Email not valid"
+                }
+            };
+        }
+
+        const msg = {
+            to: `${email}`,
+            from: 'jolf20@student.bth.se', // Use the email address or domain you verified above
+            subject: `${user} invited you to collaborate on a document`,
+            text: `${user} wants to collaborate with you on a document. Please register at www.student.bth.se/~jolf20/editor to get started!`,
+        };
+
+        console.log(msg);
+
+        (async () => {
+            try {
+              const result = await sgMail.send(msg);
+              return result;
+            } catch (error) {
+              console.error(error);
+          
+              if (error.response) {
+                
+                console.error(error.response.body)
+              }
+            }
+        })();
+    }
 };
 
 module.exports = docs;
