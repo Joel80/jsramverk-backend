5cca92486028c3e50e7499a0a18dfea1043a3e23
Joel Löfgren <jdtlofgren@gmail.com>
2022-10-14 14:49:45 +0200

    Fixed error in attributes to set for doc when saved

--- models/docs.js
+++ models/docs.js
@@ -96,7 +96,7 @@ const docs = {
 
         const filter = { _id: ObjectId(doc._id) };
 
-        const updateDoc = { $set: {name: doc.name, html: doc.html, allowed_users: doc.allowed_users, code: doc.code, comments: code.comments}, };
+        const updateDoc = { $set: {name: doc.name, html: doc.html, allowed_users: doc.allowed_users, code: doc.code, comments: doc.comments}, };
 
         try {
             db = await database.getDb();
